9ade3ea28f1b5b7f525b80073d2c0660ba365f03
haixiang <373971510@qq.com>
2020-05-22 00:11:26 +0800

    loading 下拉加载 更多

--- src/views/air/ticketList.vue
+++ src/views/air/ticketList.vue
@@ -1,27 +1,34 @@
 <template>
     <div>
-     <van-nav-bar
-        :title="cityDepArr"
-        left-text="返回"
-        right-text="按钮"
-        left-arrow
-        @click-left="$router.back()"
-        @click-right="onClickRight"
-     />
+     <van-sticky>
+        <van-nav-bar
+            :title="cityDepArr"
+            left-text="返回"
+            right-text="按钮"
+            left-arrow
+            @click-left="$router.back()"
+            @click-right="onClickRight"
+        />
+     </van-sticky>
+   
+     <van-dropdown-menu :class="{activepad:!flightList.length}">
+         <FilterFlight />
+        
+    </van-dropdown-menu>
 
-     <van-dropdown-menu>
-        <van-dropdown-item v-model="value1" :options="option1" />
-        <van-dropdown-item v-model="value2" :options="option2" />
-     </van-dropdown-menu>
-
-     <van-list
+     <van-list   
         v-model="loading"
         :finished="finished"
-        finished-text="没有更多了"
+        :finished-text="finishedtext"
         @load="onLoad"
+        :immediate-check="false"
         >
-        <ticket-item v-for="(item,index) in flightList" :key="index" :flightitem="item"></ticket-item>
+        <div v-if="flightList.length"><ticket-item v-for="(item,index) in flightList" :key="index" :flightitem="item" ></ticket-item></div>
+        <van-loading v-if="loadins" :vertical="true" color="#1989fa" />
+         <van-empty v-if="empty" description="没有找到该路程的机票" />
     </van-list>
+    
+    
      
   </div>
 </template>
@@ -29,9 +36,11 @@
 <script>
 import ticketItem from '@/components/flights/ticketItem.vue'
 import { flightsInfo } from '@/api/flights.js'
+import FilterFlight from '@/components/flights/FilterFlight'
 export default {
     components:{
-        ticketItem
+        ticketItem,
+        FilterFlight
     },
     computed: {
         cityDepArr() {
@@ -40,31 +49,19 @@ export default {
     },
     data() {
         return {
-            flightList:[],  //数据渲染
-            finished:false, //下拉
-            loading:false,  //下拉
-            bigsize:0,      //开始截取
-            endsize:10,    //结束截取
-            storesize:10,
+            empty:false,  //没找到路程机票,让他显示
+            loadins:true, //进入页面菊花图转转转,拿到数据隐藏
+            flightList:[],  //数据存储
+            finished:false, //下拉加载更多
+            finishedtext:'没有更多了',
+            loading:false,  //下拉加载更多
+            bigsize:0,      //开始截取数据下标
+            endsize:10,    //结束截取数据下标
+            storesize:10,  //暂存一夜数
             storeData:{},   //暂存数据
-            value1: 0,
-            value2: 'a',
-            option1: [
-                { text: '全部商品', value: 0 },
-                { text: '新款商品', value: 1 },
-                { text: '活动商品', value: 2 },
-            ],
-            option2: [
-                { text: '默认排序', value: 'a' },
-                { text: '好评排序', value: 'b' },
-                { text: '销量排序', value: 'c' },
-            ],
         };
     },
     methods:{
-        onClickLeft() {
-
-        },
         onClickRight() {
 
         },
@@ -78,6 +75,18 @@ export default {
         handleFlight() {
             flightsInfo(this.$route.query).then(res => {
                 this.storeData = res.data
+                if(res.data.code == 200) {
+                    setTimeout(() => {
+                        this.loadins = false
+                        if(!res.data.data.length) {
+                            this.empty = true 
+                            this.finishedtext = ''
+                        }
+                        
+                        
+                    },1000)
+                }
+                
                 setTimeout(() => {
                     this.flightList.push(...this.storeData.data.slice(this.bigsize,this.endsize))
                     this.loading = false
@@ -97,5 +106,8 @@ export default {
 <style lang="less" scoped>
 /deep/ .van-nav-bar__title{
     font-weight: 700;
+}
+.activepad{
+    padding-bottom: 49.556vw;
 }
 </style>
