243c7c6947506fa42cdebf55c891b06d49c55890
haixiang <373971510@qq.com>
2020-05-24 16:42:40 +0800

    pay界面结构完成

--- src/components/Pay/Payinfo_3_person.vue
+++ src/components/Pay/Payinfo_3_person.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="person-pay">
       <van-cell-group style="margin-bottom:5px;" v-for="(item,index) in person" :key="index">
-        <van-field label="乘机人" class-prefix="add-o"  readonly  @click="addFlight" />
-        <van-field label="姓名"   value="" placeholder="与乘机证件一致" />
-        <van-field label="身份证" value="" placeholder="请填写证件号码" />
+        <van-field label="乘机人" @click-left-icon="addPerson" @click-right-icon="delPerson(index)" left-icon="add-o" right-icon="close" readonly></van-field>
+        <van-field label="姓名"  v-model="item.name" placeholder="与乘机证件一致" />
+        <van-field label="身份证" v-model="item.card" maxlength="18" value="" placeholder="请填写证件号码" />
       </van-cell-group>
+      <van-cell-group style="margin-top:25px;">
+        <van-field label="联系人" left-icon="friends-o" readonly></van-field>
+        <van-field label="姓名"  placeholder="接收机票联系人" />
+        <van-field label="手机号" placeholder="接收机票手机" >
+            <template #button>
+                <van-button size="small" type="primary">发送验证码</van-button>
+            </template> 
+        </van-field>
+      </van-cell-group>
+      <van-submit-bar :price="3050" button-text="提交订单"  color="#be99ff" @submit="onSubmit" />
   </div>
 </template>
 
@@ -16,8 +26,18 @@ export default {
         }
     },
     methods:{
-        addFlight() {
+        addPerson() {
             this.person.push({})
+        },
+        delPerson(index) {
+            if(this.person.length == 1) {
+                this.$msg.fail('最少保留一张车票')
+                return
+            }
+            this.person.splice(index,1)
+        },
+        onSubmit() {
+            
         }
     }
 }
