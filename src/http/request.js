096d9935956cf22a286a885b27aa38bd44a81b4a
shelili <964384056@qq.com>
2020-05-25 16:25:18 +0800

    设置响应拦截器

--- src/http/request.js
+++ src/http/request.js
@@ -1,7 +1,18 @@
 import axios from 'axios'
+import { Toast } from 'vant'
 
 const request = axios.create({
-    baseURL:'http://localhost:1337/api'
+    baseURL: 'http://localhost:1337/api'
 })
 
 export default request
+
+// 响应拦截器
+request.interceptors.response.use(res => {
+    const { msg, code } = res.data
+    const errerrRegExp = /^3\d{2}$/;
+    if (msg && errerrRegExp.test(code)) {
+        Toast.fail(msg)
+    }
+    return res
+})
