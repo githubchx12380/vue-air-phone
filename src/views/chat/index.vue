4060158a2059b5e145ff270790aa6a884570fe80
haixiang <373971510@qq.com>
2020-06-15 15:41:46 +0800

    聊天页面样式

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -20,76 +20,7 @@
           </div>
           <img src="../../assets/img/head_img.png" alt />
         </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
-        <div class="chat_zi">
-          <div class="chat_main">
-            <p class="name">李某</p>
-            <p class="content">你说什么呢我来个擦水水</p>
-          </div>
-          <img src="../../assets/img/head_img.png" alt />
-        </div>
+     
       </van-pull-refresh>
 
     <div class="char_bom">
@@ -128,65 +59,65 @@ export default {
 <style scoped lang="less">
 .chat {
   .van-nav-bar {
-    border-bottom: 1px solid #eee;
+    border-bottom: 0.278vw solid #eee;
   }
    .refresh {
     overflow: auto;
-    margin:46px 0 50px 0;
-    padding: 10px;
-    height: calc(100vh - 46px - 52px);
+    margin:12.778vw 0 13.889vw 0;
+    padding: 2.778vw;
+    height: calc(100vh - 12.778vw - 14.444vw);
       .chat_br {
-        margin-bottom: 15px;
+        margin-bottom: 4.167vw;
         display: flex;
         img {
-          margin-right: 10px;
-          width: 40px;
-          height: 40px;
+          margin-right: 2.778vw;
+          width: 11.111vw;
+          height: 11.111vw;
         }
         .chat_main {
-          padding-right: 60px;
+          padding-right: 16.667vw;
           flex: 1;
           .name {
             font-family: sans-serif;
             color: #555;
-            font-size: 15px;
-            margin-bottom: 5px;
+            font-size: 4.167vw;
+            margin-bottom: 1.389vw;
           }
           .content {
             display: inline-block;
             background-color: #1a90ea;
             color: white;
-            border-radius: 12px;
-            padding: 10px;
+            border-radius: 3.333vw;
+            padding: 2.778vw;
             font-family: sans-serif;
           }
         }
       }
       .chat_zi {
         display: flex;
-         margin-bottom: 15px;
+         margin-bottom: 4.167vw;
         img {
-          margin-left: 10px;
-          width: 40px;
-          height: 40px;
+          margin-left: 2.778vw;
+          width: 11.111vw;
+          height: 11.111vw;
           border-radius: 50%;
         }
         .chat_main {
           flex: 1;
-          padding-left: 60px;
+          padding-left: 16.667vw;
           text-align: right;
           .name {
             font-family: sans-serif;
             color: #555;
-            font-size: 15px;
-            margin-bottom: 5px;
+            font-size: 4.167vw;
+            margin-bottom: 1.389vw;
           }
           .content {
             display: inline-block;
             background-color: #1a90ea;
             color: white;
-            border-radius: 12px;
-            padding: 10px;
+            border-radius: 3.333vw;
+            padding: 2.778vw;
             font-family: sans-serif;
           }
         }
@@ -200,30 +131,29 @@ export default {
     left: 0;
     right: 0;
     display: flex;
-    height: 50px;
-    border-top: 1px solid #eee;
+    border-top: 0.278vw solid #eee;
     align-items: center;
-    padding: 0 10px;
+    padding: 2.778vw 2.778vw;
      >div{
       height: 100%;
       display: flex;
       align-items: center;
     }
     .icons{
-      font-size: 32px;
+      font-size: 8.889vw;
       &:nth-child(1) {
-        margin-right: 8px;
+        margin-right: 2.222vw;
       }
     }
     .vant_field{
       padding: 0;
-      padding: 0 10px;
+      padding: 0 2.778vw;
       /deep/ .van-cell__value{
-        border:1px solid #ddd;
-        padding: 3px 0;
-        border-radius: 5px;
+        border:0.278vw solid #ddd;
+        padding: 0.833vw 0;
+        border-radius: 1.389vw;
         .van-field__control{
-          padding: 0 10px;
+          padding: 0 2.778vw;
         }
       }
     }
