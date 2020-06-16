3ca08ddc27c8c9ee2ebc8f5380b2f59c65581578
haixiang <373971510@qq.com>
2020-06-16 20:21:50 +0800

    连接服务端

--- src/views/chat/socket.js
+++ src/views/chat/socket.js
@@ -1,8 +1,11 @@
 import io from 'socket.io-client'
 const socket = io('http://localhost:1337')
 let id = localStorage.getItem('userId')
+
 import store from '../../store/index'
 export const  now = () => {
+    
+    
     socket.emit('now',{msg:'正在输入..',id})
 }
 
@@ -15,7 +18,9 @@ export const emitContent = (value) => {
 } 
 
 socket.on('hehe',data => {
-  store.commit('chat/ADD_CHAT',data)
+    console.log(data);
+    
+//   store.commit('chat/ADD_CHAT',data)
 }) 
 
 
